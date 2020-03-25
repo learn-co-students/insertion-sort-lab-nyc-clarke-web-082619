032410c3e120a763eab73cdc5ed5a506daf2a4c4
jw20191n <clairejw1015@gmail.com>
2020-03-24 20:17:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let index = 0;
 
+    for (let i=0; i < array.length; i++){
+        if (min > array[i]){
+            min = array[i];
+            index = i;
+        }
+    }
+    array.splice(index, 1);
+    return min;
 }
 
-function insertionSort(array){
+function insertionSort(array){  
+    let newArray = [];
+    let newMin;
 
+    while(array.length != 0){
+        newMin = findMinAndRemove(array);
+        newArray.push(newMin);
+    }
+    return newArray;
 }
